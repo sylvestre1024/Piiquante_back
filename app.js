a307e95665c2dbcbaef29d9b574c5ca3acdacb19
Sylvestre <sylvestre1024@gmail.com>
2022-09-21 16:30:50 +0200

    Maj - token

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 app.use(express.json({ limit: "1mb" }));
 
 // Prevention of NoSQL injection
-app.use(mongoSanitize());
+/*app.use(mongoSanitize());*/
 
 // Use of routes
 app.use('/images', express.static(path.join(__dirname, 'images')));
